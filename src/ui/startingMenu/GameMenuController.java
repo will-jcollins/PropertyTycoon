aa689ee71300000ed9481825a1a502d57d81df36
Max <pk316@sussex.ac.uk>
2022-04-25 13:51:42 +0100

    Added CSS, FXML and mainManu file

---
+++ src/ui/startingMenu/GameMenuController.java
@@ -0,0 +1,61 @@
+package ui.startingMenu;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * Controller to gameMode.fxml file
+ */
+public class GameMenuController
+{
+    /**
+     * Starts the classical mode game
+     * @param event
+     */
+    //TODO start game in classical mode
+    public void classicalMode(javafx.event.ActionEvent event)
+    {
+
+    }
+
+    public void returnButton(javafx.event.ActionEvent event)
+    {
+        try{
+            ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../FXML files/playerMenu.fxml"));
+            Scene scene = new Scene(loader.load(),800,600 );
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Directs user to timeFormatMenu.fxml where the user can choose the time format in which they want to play the game
+     * @param event
+     */
+    public void timeMode(javafx.event.ActionEvent event)
+    {
+        try{
+            ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../FXML files/timeFormatMenu.fxml"));
+            Scene scene = new Scene(loader.load(),300,400 );
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/ui/startingMenu/Main.java
@@ -0,0 +1,32 @@
+package ui.startingMenu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Class which starts the Main menu and the game
+ */
+public class Main extends Application {
+
+
+    /**
+     * reading the first fxml file.
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../assets/FXML files/mainMenu.fxml"));
+        primaryStage.setTitle("Start");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
